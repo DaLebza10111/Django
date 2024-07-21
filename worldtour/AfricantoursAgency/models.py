5879500b392bf7e7aced285c4c8bc69ca1832df9
DaLebza10111 <lebohangsopazi3@gmail.com>
2024-07-21 09:02:21 +0200

    Adding demo records

--- worldtour/AfricantoursAgency/models.py
+++ worldtour/AfricantoursAgency/models.py
@@ -7,3 +7,7 @@ class Tour(models.Model):
     country_of_destination = models.CharField(max_length=64)
     number_of_nights = models.IntegerField()
     price = models.IntegerField()
+
+    #string representation of the records
+    def __str__(self) -> str:
+        return (f"ID:{self.id} from {self.country_of_origin} to {self.country_of_destination} duration {self.number_of_nights} nights and the total cost ${self.price}")
