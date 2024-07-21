14bf5bcb1a7c48b86cc454a6eff0e3ce6fc1b04b
DaLebza10111 <lebohangsopazi3@gmail.com>
2024-07-21 08:22:27 +0200

    Apply migrations

--- worldtour/AfricantoursAgency/models.py
+++ worldtour/AfricantoursAgency/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    #Origin country and destination, Number of nights, price
+    country_of_origin = models.CharField(max_length=64)
+    country_of_destination = models.CharField(max_length=64)
+    number_of_nights = models.IntegerField()
+    price = models.IntegerField()
